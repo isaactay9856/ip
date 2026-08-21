b386788d46d586daa5787cb11bc639b8c209ac1e
isaactay9856 <isaactaynotes@gmail.com>
2026-08-22 02:36:10 +0800

    Made Week 2 Stage 1 changes to MEKA.java

--- src/main/java/MEKA.java
+++ src/main/java/MEKA.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+
+/**
+ * A simple chatbot that echoes commands until the user says goodbye.
+ */
 public class MEKA {
     public static void main(String[] args) {
         String banner = "███╗   ███╗███████╗██╗  ██╗ █████╗\n"
@@ -7,6 +12,28 @@ public class MEKA {
                 + "██║ ╚═╝ ██║███████╗██║  ██╗██║  ██║\n"
                 + "╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝\n";
 
+        String seperator = "____________________________________________________________";
+
+        System.out.println(seperator);
         System.out.println(banner);
+        System.out.println(" Hello! I'm MEKA.");
+        System.out.println(" What can I do for you?");
+        System.out.println(seperator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println(seperator);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(seperator);
+                break;
+            }
+
+            System.out.println(seperator);
+            System.out.println(" " + command);
+            System.out.println(seperator);
+        }
     }
 }
